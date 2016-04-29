7f219e290494128cb9dfacc45fb2acb7df80c306
PaulKnoops <knoops93@gmail.com>
2016-04-29 08:34:58 -0400

    Create R_code.R

---
+++ R_code.R
@@ -0,0 +1,123 @@
+#install.packages("dplyr")
+library(dplyr)
+library(lme4)
+library(effects)
+
+# Bring in the data for mature females (copulation) and Immautre females (courtship)
+copulation <- read.csv("Mature.csv",h=T)
+courtship <- read.csv("Immature.csv",h=T)
+
+# Create unique Fly_ID for each individual with Box (treatment), date, replicate, and vial Number
+courtship$Fly_ID <- with(courtship, paste0(courtship$Box,courtship$Date,courtship$Replicate, courtship$Vial_number))
+courtship$Fly_ID
+
+# Create Delta BP
+courtship$deltaBP <- (courtship$BP.12.00.am - courtship$BP.8.00.Am)
+
+# Change time (in HH:MM:SS) format to seconds (One had a value of -1, not sure, but changed to 0)
+courtship$startTimeSeconds <- (courtship$trial_latency_behav_end - courtship$court_duration)
+courtship$startTimeSeconds[1339]
+courtship$startTimeSeconds[1339] = 0
+
+# Create new column of relative values for courtship start times (i.e so Observation all start at Time = 0)
+courtship$relativeStartTimeSeconds <- (courtship$startTimeSeconds - courtship$Observation.Initiation)
+courtship$relativeStartTimeSeconds
+
+# New column for relative values of trial duration at end of behaviour
+courtship$relativeTrial_latency_end <- (courtship$relativeStartTimeSeconds + courtship$court_duration)
+courtship$relativeTrial_latency_end
+
+# Need to get all courtship under 900 seconds (relative court duration)
+#First, transition step for finding values ending abov 900
+courtship$nineHundredTransition <- (900 - courtship$relativeTrial_latency_end)
+# Second, if value for nineHundredTransition is negative, equate it to 900, all else stay as relativeTrial_latency_end
+courtship$relativeTrialLatency900 <- ifelse(courtship$nineHundredTransition<0,900,courtship$relativeTrial_latency_end)
+# Third, relative courtship duration (not including values over 900)
+courtship$relativeCourtDuration <- (courtship$relativeTrialLatency900 - courtship$relativeStartTimeSeconds)
+
+head(courtship)
+
+startLess900 <- subset(courtship, relativeCourtDuration>0)
+summary(startLess900)
+
+# Create a data frame for the predictor variables and for Fly_ID
+pred_var_dat <- subset(courtship, select = c(Box, Date, Replicate, Vial_number, Temp, Humidity, BP.12.00.am, BP.8.00.Am, BP.Room,
+                                             Observation.Initiation, Fly_ID, deltaBP))
+# Make data frame only include unique values (i.e. remove duplicate rows)
+pred_var_dat <- unique(pred_var_dat) 
+
+# Create a group by Fly_Id
+FlyID <- group_by(startLess900,Fly_ID)
+head(FlyID)
+
+courtSum <- summarise(FlyID, sum = sum(relativeCourtDuration), count = n())
+head(courtSum)
+courtSum
+
+
+# Currently courtSum is a dplyr data table (tbl_df). Not sure if it will merge properly with a regular data frame
+# Seems to work
+courtship_for_analysis <- merge(x = pred_var_dat, y = courtSum, by.x="Fly_ID", by.y="Fly_ID")
+
+with(courtship_for_analysis,
+     boxplot(sum ~ Box))
+
+with(courtship_for_analysis,
+     boxplot(sum ~ Date))
+
+
+# A simple version of the analysis
+
+courtship_for_analysis$TempCent <- scale(courtship_for_analysis$Temp, scale=F)
+courtship_for_analysis$HumCent <- scale(courtship_for_analysis$Humidity, scale = F)
+courtship_for_analysis$BPCent <- scale(courtship_for_analysis$deltaBP, scale = F)
+
+courtship_model1 <- lmer(sum ~ Box + Replicate + TempCent + HumCent + BPCent + (1|Date), data = courtship_for_analysis)
+
+courtship_model2 <- lmer(count ~ Box + Replicate + TempCent + HumCent + BPCent + (1|Date), data = courtship_for_analysis)
+
+summary(courtship_model1)
+summary(courtship_model2)
+
+plot(allEffects(courtship_model1))
+
+plot(effect("Box", courtship_model1))
+plot(effect("Box", courtship_model2))
+
+# Make total proportion of time courting
+
+
+# Copulation Analysis
+
+summary(copulation)
+dim(copulation)
+head(copulation)
+copulation$deltaBP <- (copulation$BP.12.00.am - copulation$BP.8.00.Am)
+cop_data <- subset(copulation, select = c(Box, Date, Replicate, Vial.., Temp, Humidity, BP.12.00.am, BP.8.00.Am, BP.Room, Fly_ID, deltaBP))
+cop_data <- unique(cop_data)
+head(cop_data)
+
+LatCop <- distinct(select(copulation, Cop_latency, Cop_Duration, Fly_ID))
+head(LatCop)
+
+copul_for_analysis <- merge(x = cop_data, y = LatCop, by.x="Fly_ID", by.y="Fly_ID")
+
+copul_for_analysis$TempCent <- scale(copul_for_analysis$Temp, scale=F)
+copul_for_analysis$HumCent <- scale(copul_for_analysis$Humidity, scale = F)
+copul_for_analysis$BPCent <- scale(copul_for_analysis$deltaBP, scale = F)
+
+
+copul_model1 <- lmer(Cop_latency ~ Box + Replicate + TempCent + HumCent + BPCent + (1|Date), data = copul_for_analysis)
+
+copul_model2 <- lmer(Cop_Duration ~ Box + Replicate + TempCent + HumCent + BPCent + (1|Date), data = copul_for_analysis)
+
+summary(copul_model1)
+summary(copul_model2)
+
+with(copul_for_analysis, boxplot(Cop_latency ~ Box))
+with(copul_for_analysis, boxplot(Cop_Duration ~ Box))
+
+plot(allEffects(copul_model1))
+
+plot(effect("Box", copul_model1))
+plot(effect("Box", copul_model2))
